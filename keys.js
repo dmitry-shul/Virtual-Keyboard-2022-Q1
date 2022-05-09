fdacde71423532d986a3160fb2ae3d35c633874b
Dmitry Shulzhytski <matemV@mail.ru>
2022-05-09 22:25:35 +0300

    feat: add keyboard

---
+++ keys.js
@@ -0,0 +1,68 @@
+//en
+export const ENcaseDown = [
+  ["`","1","2","3","4","5","6","7","8","9","0","-","=","&larr;"], 
+  ["Tab","q","w","e","r","t","y","u","i","o","p","[","]","\\","Del"], 
+  ["CapsLock","a","s","d","f","g","h","j","k","l",";","'","ENTER"], 
+  ["Shift","z","x","c","v","b","n","m",",",".","/","&#9650;","Shift"], 
+  ["Ctrl","En","Alt","","Alt","&#9668;","&#9660;","&#9658;","Ctrl"]
+];
+export const ENshiftKey = [
+  ["~","!","@","#","$","%","^","&","*","(",")","_","+","&larr;"], 
+  ["Tab","Q","W","E","R","T","Y","U","I","O","P","{","}","|","Del"], 
+  ["CapsLock","A","S","D","F","G","H","J","K","L",":",`"`,"ENTER"], 
+  ["Shift","Z","X","C","V","B","N","M","<",">","?","&#9650;","Shift"], 
+  ["Ctrl","En","Alt","","Alt","&#9668;","&#9660;","&#9658;","Ctrl"]
+];
+export const ENcaps = [
+  ["`","1","2","3","4","5","6","7","8","9","0","-","=","&larr;"], 
+  ["Tab","Q","W","E","R","T","Y","U","I","O","P","[","]","\\","Del"], 
+  ["CapsLock","A","S","D","F","G","H","J","K","L",";","'","ENTER"], 
+  ["Shift","Z","X","C","V","B","N","M",",",".","/","&#9650;","Shift"], 
+  ["Ctrl","En","Alt","","Alt","&#9668;","&#9660;","&#9658;","Ctrl"]
+];
+export const ENshiftCaps = [
+  ["~","!","@","#","$","%","^","&","*","(",")","_","+","&larr;"], 
+  ["Tab","q","w","e","r","t","y","u","i","o","p","{","}","|","Del"], 
+  ["CapsLock","a","s","d","f","g","h","j","k","l",":",`"`,"ENTER"], 
+  ["Shift","z","x","c","v","b","n","m","<",">","?","&#9650;","Shift"], 
+  ["Ctrl","En","Alt","","Alt","&#9668;","&#9660;","&#9658;","Ctrl"]
+];
+
+//ru
+export const RUcaseDown = [
+  ["ё","1","2","3","4","5","6","7","8","9","0","-","=","&larr;"], 
+  ["Tab","й","ц","у","к","е","н","г","ш","щ","з","х","ъ","\\","Del"], 
+  ["CapsLock","ф","ы","в","а","п","р","о","л","д","ж","э","ENTER"], 
+  ["Shift","я","ч","с","м","и","т","ь","б","ю",".","&#9650;","Shift"], 
+  ["Ctrl","Ru","Alt","","Alt","&#9668;","&#9660;","&#9658;","Ctrl"]
+];
+export const RUshiftKey = [
+  ["Ё","!",`"`,"№",";","%",":","?","*","(",")","_","+","&larr;"], 
+  ["Tab","Й","Ц","У","К","Е","Н","Г","Ш","Щ","З","Х","Ъ","/","Del"], 
+  ["CapsLock","Ф","Ы","В","А","П","Р","О","Л","Д","Ж","Э","ENTER"], 
+  ["Shift","Я","Ч","С","М","И","Т","Ь","Б","Ю",",","&#9650;","Shift"], 
+  ["Ctrl","Ru","Alt","","Alt","&#9668;","&#9660;","&#9658;","Ctrl"]
+];
+export const RUcaps = [
+  ["Ё","1","2","3","4","5","6","7","8","9","0","-","=","&larr;"], 
+  ["Tab","Й","Ц","У","К","Е","Н","Г","Ш","Щ","З","Х","Ъ","\\","Del"], 
+  ["CapsLock","Ф","Ы","В","А","П","Р","О","Л","Д","Ж","Э","ENTER"], 
+  ["Shift","Я","Ч","С","М","И","Т","Ь","Б","Ю",".","&#9650;","Shift"], 
+  ["Ctrl","Ru","Alt","","Alt","&#9668;","&#9660;","&#9658;","Ctrl"]
+];
+export const RUshiftCaps = [
+  ["Ё","!",`"`,"№",";","%",":","?","*","(",")","_","+","&larr;"], 
+  ["Tab","й","ц","у","к","е","н","г","ш","щ","з","х","ъ","/","Del"], 
+  ["CapsLock","ф","ы","в","а","п","р","о","л","д","ж","э","ENTER"], 
+  ["Shift","я","ч","с","м","и","т","ь","б","ю",",","&#9650;","Shift"], 
+  ["Ctrl","Ru","Alt","","Alt","&#9668;","&#9660;","&#9658;","Ctrl"]
+];
+
+//event.key
+export const eventCodes = [
+	["Backquote","Digit1","Digit2","Digit3","Digit4","Digit5","Digit6","Digit7","Digit8","Digit9","Digit0","Minus","Equal","Backspace"], 
+  ["Tab","KeyQ","KeyW","KeyE","KeyR","KeyT","KeyY","KeyU","KeyI","KeyO","KeyP","BracketLeft","BracketRight","Backslash","Delete"], 
+  ["CapsLock","KeyA","KeyS","KeyD","KeyF","KeyG","KeyH","KeyJ","KeyK","KeyL","Semicolon","Quote","Enter"], 
+  ["ShiftLeft","KeyZ","KeyX","KeyC","KeyV","KeyB","KeyN","KeyM","Comma","Period","Slash","ArrowUp","ShiftRight"], 
+  ["ControlLeft","En","AltLeft","Space","AltRight","ArrowLeft","ArrowDown","ArrowRight","ControlRight"]
+];
